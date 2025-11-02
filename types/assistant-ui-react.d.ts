a42058f0dc70cb1319bd31c8189d853fcd645465
daedu86 <daedu.rodriguez@gmail.com>
2025-11-02 21:18:21 +0100

    chore: add assistant ui type shims

---
+++ types/assistant-ui-react.d.ts
@@ -0,0 +1,7 @@
+declare module "@assistant-ui/react/runtimes/utils/MessageRepository" {
+  export * from "@assistant-ui/react/dist/runtimes/utils/MessageRepository";
+}
+
+declare module "@assistant-ui/react/runtimes/core/ThreadRuntimeCore" {
+  export * from "@assistant-ui/react/dist/runtimes/core/ThreadRuntimeCore";
+}
